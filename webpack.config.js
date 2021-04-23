c701173da9b1c8931eda3736a6d83bb1b30268ca
Luis Manzo <manzosk3@gmail.com>
2021-04-22 20:48:33 -0400

    webpack dev server bundles ts from memory

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,18 @@ let path = require('path');
 module.exports = {
   entry: `./src/index.ts`,
   module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        include: [path.resolve(__dirname, 'src')]
-      }
-    ]
+    rules: [{
+      test: /\.ts$/,
+      use: 'ts-loader',
+      include: [path.resolve(__dirname, 'src')]
+    }]
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
-  }
+    path: path.resolve(__dirname, '/public/')
+  },
+  devServer: {
+    publicPath: '/public/',
+    port: 8081,
+  },
 }
